a814f21eb9bb101bc01625b8555eca4965904900
jvblum <jvmell.skch@gmail.com>
2021-09-22 17:02:03 -0400

    imod wordSearch.js to search words vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  const transpose = function(matrix) {
+    // Put your solution here
+    let someArr = [];
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        someArr.push(new Array());
+        someArr[j].push(matrix[i][j]);
+      }
     }
-}
+    let result = [...someArr];
+    return result;
+  };
+  
+  const verticalJoin = transpose(letters).map(ls => ls.join(''))
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+    console.log(l);
+    if (l.includes(word)) return true;
+  }
+  for (l of verticalJoin) {
+    console.log(l);
+    if (l.includes(word)) return true;
+  }
+  return false
+};
 
 module.exports = wordSearch
+
+// pair: @jvblum, @hcblam
